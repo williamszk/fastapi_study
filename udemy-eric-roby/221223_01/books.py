3c3e0d992794597ba1b1e13d0f2ea95e1ffc51ee
williamszk <wllmszk@gmail.com>
2022-12-24 16:54:51 -0800

    fastapi study

--- udemy-eric-roby/221223_01/books.py
+++ udemy-eric-roby/221223_01/books.py
@@ -2,9 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+books = {
+    "book_1": {"author": "Author One", "title": "Title One"},
+    "book_2": {"author": "Author Two", "title": "Title Two"},
+    "book_3": {"author": "Author Three", "title": "Title Three"},
+    "book_4": {"author": "Author Four", "title": "Title Four"},
+    "book_5": {"author": "Author Five", "title": "Title Five"},
+}
+
+
 @app.get("/")
-async def first_api():
-    return {"message":"Hello World!"}
+async def read_all_books():
+    return books
 
 
+@app.get("/books/my_book")
+async def read_favorite_book():
+    return {"title":"My favorite book"}
 
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    query = [books[k] for k in books.keys() if books[k]["title"] == book_title]
+    if query is None:
+        return None
+    return query
