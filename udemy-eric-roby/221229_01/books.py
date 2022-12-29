d37c60b8bdf0b257a104b0a35a8936f0bfa088b1
williamszk <wllmszk@gmail.com>
2022-12-29 13:44:13 -0800

    fastapi coding with roby, books2 project

---
+++ udemy-eric-roby/221229_01/books.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
+
+app = FastAPI()
+
+
+class Book(BaseModel):
+    id: UUID
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+books: list[dict] = []
+
+
+@app.get("/")
+async def read_all_books():
+    return books
---
+++ udemy-eric-roby/221229_01/readme.md
@@ -0,0 +1,5 @@
+
+
+This is books 2 project.
+
+
