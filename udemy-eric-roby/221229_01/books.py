6a2356a4443b352fe61820bef8b002097d003d66
williamszk <wllmszk@gmail.com>
2022-12-30 15:51:45 -0800

    fastapi study, pydantic input checks

--- udemy-eric-roby/221229_01/books.py
+++ udemy-eric-roby/221229_01/books.py
@@ -1,21 +1,31 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import UUID
+from typing import Optional
 
 app = FastAPI()
 
 
 class Book(BaseModel):
     id: UUID
-    title: str
+    title: str = Field(min_length=1)
     author: str
-    description: str
+    description: Optional[str] = Field(
+        title="Description of Book", max_length=100, min_length=1
+    )
     rating: int
 
 
-books: list[dict] = []
+BOOKS: list[Book] = []
 
 
 @app.get("/")
 async def read_all_books():
-    return books
+    return BOOKS
+
+
+@app.post("/")
+async def create_book(book: Book):
+    BOOKS.append(book)
+
+    return book
